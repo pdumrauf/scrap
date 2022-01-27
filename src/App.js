fd4a29e69fd503dc686dae858a8e370182ffcf5c
pdumrauf <dumraufp@gmail.com>
2022-01-27 18:39:49 +0100

    itemCount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import NavBar from './components/NavBar.js'
-import ItemListContainer from './components/ItemListContainer.js'
+import NavBar from './components/NavBar.js';
+import ItemListContainer from './components/ItemListContainer.js';
 
 function App() {
     return (
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,17 @@
+import ItemCount from './ItemCount.js'
+
 function ItemListContainer ({greeting}) {
+
+  function addItems(counter) {
+    console.log('product added.')
+  }
+
   return (
-    <h1>{greeting}</h1>
+    <div>
+      <h1>{greeting}</h1>
+      <ItemCount stock={5} initial={1} onAdd={addItems} />
+    </div>
   );
 }
 
 export default ItemListContainer;
-
